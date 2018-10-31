f1c8d5bf5f1b350033d20b96bf7455efbad0b363
miina <hallaelsa@hotmail.com>
2018-10-31 08:47:21 +0100

    link to page section works

--- s315579IndividuellOppgave/ClientApp/src/components/Content.js
+++ s315579IndividuellOppgave/ClientApp/src/components/Content.js
@@ -9,14 +9,14 @@ export default class Content extends Component {
 
         return (
             <div className="contents-wrapper" >
-                <h2>{group.parent.title}</h2>
+                <h2 id={group.parent.id}>{group.parent.title}</h2>
 
                 {group.categories.map((category) => {
                     const filteredQA = faqs.filter((aq) => aq.category.id === category.id);
                     
                     return (
                         <div key={category.id + category.title}>
-                            <h4>{category.title}</h4>
+                            <h4 id={category.id}>{category.title}</h4>
 
                             {filteredQA.map((faq) => {
                                 return (
